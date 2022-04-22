ef888a4e2b1a34e2ab834d6ee9f469e885b31ae4
AdeshMask <adeshmaske255@gmail.com>
2022-04-22 20:23:54 +0530

    Updated

---
+++ SQL_Queries2.sql
@@ -0,0 +1,43 @@
+create database Address_Book_DB;
+
+use Address_Book_DB;
+
+create table Address_Book(Book_ID int primary key, book_name varchar (50));
+
+desc Address_Book;
+
+create table Book1(book_id int primary key,first_name varchar(25),last_name varchar(25),address varchar(100),city varchar(25),state varchar(25),
+					zip int,phone_no long,email varchar(30),foreign key(book_id) references Address_Book(Book_ID));
+
+create table Book2(book_id int primary key,first_name varchar(25),last_name varchar(25),address varchar(100),city varchar(25),state varchar(25),
+					zip int,phone_no long,email varchar(30),foreign key(book_id) references Address_Book(Book_ID));
+
+insert into Book1(book_id,first_name,last_name,address,city,state,zip,phone_no,email) values ('1','Adesh','Maske','Jotibawadi','Solapur','MH','413406','1231212333','adesh@gmail.com' );
+insert into Book1(book_id,first_name,last_name,address,city,state,zip,phone_no,email) values ('2','Abc','pqr','pqrs','pqrst','Mh','321123','8979878979','abc@gmail.com');
+insert into Book1(book_id,first_name,last_name,address,city,state,zip,phone_no,email) values ('3','Xyz','Xyz123','xyz12','xyz21','MH','312332','9876543209','xyz@gmail.com');
+
+select * from Book1;
+
+update Book1 set phone_no = "9898879098" where first_name = "Abc";
+
+select city, count(city) from Book1 group by city;
+
+select state, count(state) from Book1 group by state;
+
+insert into Address_Book(Book_ID,book_name) values('1','Book1'),('2','Book2');
+select * from Address_Book;   
+
+create table contact_type(book_id int primary key,book_type varchar(20));
+
+alter table Address_Book add type varchar(25);
+
+select * from Address_Book;
+
+update Address_Book set type = 'Family' where book_name = 'book2';
+
+select * from Book1;
+insert into Book2(book_id,first_name,last_name,address,city,state,zip,phone_no,email) values ('1','Amar','Maske','Pune','haveli','Mh','433123','9087687879','amarmaske@gmail.com');
+insert into Book2(book_id,first_name,last_name,address,city,state,zip,phone_no,email) values ('2','Atul','Maske','solapur','Pune','Mh','433223','9798787879','atulmaske@gmail.com');
+select * from Book2;
+
+alter table contact_type add foreign key(book_id) references Book2(book_id);
---
+++ Sql_Queries1.sql
@@ -0,0 +1,57 @@
+create database Employee_Payroll;
+show databases;
+use Employee_Payroll;
+show tables;
+create table employee_payroll 
+(
+EmployeeID int NOT NULL AUTO_INCREMENT,
+Employee_Name varchar(255),
+Phone_number varchar(255),
+Address varchar(255),
+Department varchar(255),
+Gender char(1),
+Basic_Pay float,
+Deduction float,
+Taxable_Pay float,
+Tax float,
+NetPay float,
+Start_Date Date,
+city varchar(255),
+country varchar(255),
+PRIMARY KEY (EmployeeID)
+);
+ desc employee_payroll;
+ 
+insert into employee_payroll (Employee_Name, Phone_number,Address, Department, Gender, Basic_Pay, Deduction, Taxable_Pay, Tax, NetPay, Start_Date, city, country) values ('Adesh', '1234567890','Solapur','Engg', 'M', '30000.0', '2000.0', '1000.0', '200.0', '1800.0', '2022-03-02', 'Pune', 'India');
+insert into employee_payroll (Employee_Name, Phone_number,Address, Department, Gender, Basic_Pay, Deduction, Taxable_Pay, Tax, NetPay, Start_Date, city, country) values ("Gunjan", "1234567890", "Mumbai", "Engg", "M", '30000.0', '2000.0', '1000.0', '200.0', '1800.0', '2022-04-05', "pune", "India");
+select * from employee_payroll;
+delete from employee_payroll where EmployeeID = '3';
+insert into employee_payroll (Employee_Name, Phone_number,Address, Department, Gender, Basic_Pay, Deduction, Taxable_Pay, Tax, NetPay, Start_Date, city, country) values ('Krunali', '9165389801','Nagpur','Sales', 'F', '40000.0', '2500.0', '1500.0', '220.0', '2000.0', '2021-09-015', 'Nagpur', 'India');
+insert into employee_payroll (Employee_Name, Phone_number,Address, Department, Gender, Basic_Pay, Deduction, Taxable_Pay, Tax, NetPay, Start_Date, city, country) values ("Dhanshri", "7890123456", "Mumbai", "Marketing", "F", '35000.0', '2200.0', '1400.0', '250.0', '1900.0', '2022-03-025', "Mumbai", "India");
+insert into employee_payroll (Employee_Name, Phone_number,Address, Department, Gender, Basic_Pay, Deduction, Taxable_Pay, Tax, NetPay, Start_Date, city, country) values ('Krunali', '9165389801','Nagpur','Marketing', 'F', '40000.0', '2500.0', '1500.0', '220.0', '2000.0', '2021-09-015', 'Nagpur', 'India');
+select * from employee_payroll where Employee_Name = 'Krunali';
+alter table employee_payroll add column Salary float after NetPay;
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+select sum(Basic_pay + Deduction + Taxable_Pay + Tax + NetPay) as 'Adesh' from employee_payroll;
+select sum(Basic_pay + Deduction + Taxable_Pay + Tax + NetPay) as salary from employee_payroll;
+update employee_payroll set salary = 'sum(Basic_pay + Deduction + Taxable_Pay + Tax + NetPay)' where Employee_Name = 'Adesh';
+select Employee_Name,Phone_number,(Basic_pay + Deduction + Taxable_Pay + Tax + NetPay) as salary from employee_payroll;
+create table salary as select EmployeeID,Employee_Name,salary(Basic_pay + Deduction + Taxable_Pay + Tax + NetPay) from employee_payroll;
+update employee_payroll set salary = '35000' where Employee_Name = 'Adesh';
+update employee_payroll set salary = '35000' where Employee_Name = 'Gunjan';
+update employee_payroll set salary = '46220' where Employee_Name = 'Krunali';
+update employee_payroll set salary = '40750' where Employee_Name = 'Dhanshri';
+update employee_payroll set salary = '46220' where Employee_Name = 'Krunali';
+select sum(salary) from employee_payroll where Gender = 'F' group by Gender;
+select sum(salary) from employee_payroll where Gender = 'M' group by Gender;
+select count(*) from employee_payroll where Gender = 'F';
+select count(*) from employee_payroll where Gender = 'M';
+select avg(salary) from employee_payroll where Gender = 'M' group by Gender;
+select avg(salary) from employee_payroll where Gender = 'F' group by Gender;
+select min(salary) from employee_payroll where Gender = 'M' group by Gender;
+select max(salary) from employee_payroll where Gender = 'M' group by Gender;
+select min(salary) from employee_payroll where Gender = 'F' group by Gender;
+select max(salary) from employee_payroll where Gender = 'F' group by Gender;
+/////////////////////////////////////////////////////////////////////////////////////////////
+select * from employee_payroll;
+SELECT * FROM employee_payroll WHERE Start_Date > '2021-12-28'; 
